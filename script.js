841d54699076b38102fa5bf9cb7c332479df0867
g-briele12 <127758417+g-briele12@users.noreply.github.com>
2024-08-23 08:38:48 -0300

    site

--- script.js
+++ script.js
@@ -1,51 +1,61 @@
-const caixaPrincipal = document.querySelector("caixa-principal");
-const caixaPerguntas = document.querySelector("caixa-perguntas");
-const caixaAlternativas = document.querySelector("caixa-alternativas");
-const caixaResultado = document.querySelector("caixa-resultado");
-const textoResultado = document.querySelector("texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado:""
+        enunciado:"como você acha que as escolas poderiam integrar melhor o cuidado com os animais ao curriculo, promovendo tanto a educação quanto o envolvimento em esportes?",
         alternativas: [
-""
-""
-        ]
-    },
+            {
+                texto:"incluir atividades esportivas que envolvam cuidados com animais, como equitação ou corrida com cães.",
+                afirmacao: "a integração de esportes que envolvem animais pode proporcionar aos alunos experiências práticas de responsabilidade e cuidado. "
+            },
     {
-        enunciado: ""
-        alternativas:[
-
-
-    ]
- },
-  {
-    enunciado:""
-    alternativas:[
-        ""
-        ""
-      ]
+        texto:"criar projetos educativos que permitam aos alunos aprender sobre diferentes espécies e seu ambiente natural.",
+        afirmacao:"projetos educativos sobre animais podem estimular o interesse dos alunos pela biologia e conservção, complementando seu aprendizado acadêmico."
+    },
+]
     }
 ];
+    
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
-    perguntaAtual = perguntas [atual];
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
+    textoResultado.textContent = "";
     mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener('click', ()=> respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);        
     }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal = afirmacao;
+    atual++
+    mostraPergunta();
+}
 
-    function mostraAlternativas() {
-        for (const alternativa of perguntaAtual.alternativas){
-            const botaoAlternativas = document.createElement("button");
-            botaoAlternativas.textContent = alternativa;
-            caixaAlternativas.appendChild(botaoAlternativas);
+mostraPergunta();
 
-        }
-            
-        }
-            
-        mostraPergunta();
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2030...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
